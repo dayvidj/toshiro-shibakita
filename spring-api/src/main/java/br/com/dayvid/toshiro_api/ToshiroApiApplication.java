bd253eb52e94636f0af9994f4be9eb0f0f48f6eb
Dayvid Jonathan <dayvidaf7@gmail.com>
2026-01-09 16:10:47 -0300

    config: configure H2 datasource for local dev

---
+++ spring-api/src/main/java/br/com/dayvid/toshiro_api/ToshiroApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.dayvid.toshiro_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToshiroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToshiroApiApplication.class, args);
+	}
+
+}
---
+++ spring-api/src/test/java/br/com/dayvid/toshiro_api/ToshiroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dayvid.toshiro_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToshiroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
